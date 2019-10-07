74247a1e005e01f5f6a8cf0f21fe55edeee85db1
B-Musick <bmuze1@gmail.com>
2019-10-07 17:06:50 -0600

    Game plays through and stops when someone won

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ class Tile extends React.Component{
     handleClick=(e)=>{
         e.preventDefault();
         // Pass the index which was clicked up to the Board component to change state
-        if(this.props.letter===""){
+        console.log(this.props.gameOver)
+        if (!this.props.gameOver.playerWon && this.props.letter === ""){
             // If it is an empty space then let a letter be placed at this index
             this.props.handleClick(this.props.index);
         }
@@ -17,16 +18,15 @@ class Tile extends React.Component{
         
     
     render(){
-        let tileWidth=100;
-        let xCoord = (this.props.index) % 3 * tileWidth + "";
+        let tileWidth=100; // Used to get x and y coordinates, set text alignment
+        let xCoord = (this.props.index) % 3 * tileWidth + ""; // modulo since needs to go to 0 when get to next 'row'
         let yCoord = (Math.floor((this.props.index) / 3)) * tileWidth + "";
-        let letterColor = this.props.letter === 'X' ? 'red':'green';
+        let letterColor = this.props.letter === 'X' ? 'rgb(145, 119, 206)' :'rgb(119, 206, 122)'; // Determine color of letter
         return(
             <g>
                 <rect onClick={this.handleClick} id={"tile-" + this.props.index} x={xCoord} y={yCoord}></rect>
                 <text textLength={'100px'} x={parseInt(xCoord) + 15 + ""} y={parseInt(yCoord) + (tileWidth-10)  + ""} style={{ fill:letterColor}}>{this.props.letter}</text>
             </g>
-
         )
     }
 }
@@ -34,10 +34,22 @@ class Board extends React.Component{
     // This will loop through the board values and pass them to the Tile Components
     state = { 
         board: ["", "", "", "", "", "", "", "", ""],
-        player: Math.ceil(Math.random()*10) <=5 ? true: false // player1 = true, player2=false
+        player: Math.ceil(Math.random() * 10) <= 5 ? true : false,// player1 = true, player2=false
+        winningIndices:[
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 5]
+        ]
     }
 
     handleClick=(index)=>{
+        
+        // Passed into Tile, retrieves tile index when that tile clicked and doesnt contain a letter yet
         let letter = this.state.player ? 'X': 'O'; // This will determine who is playing
         
         this.setState(state => {
@@ -55,20 +67,54 @@ class Board extends React.Component{
             return {
                 board,
                 player
-            };
-            
-            
-            
-
+            };  
         });
         console.log(this.state.board)
     }
+
     mapTiles=()=>{
+        
+        // Used to map the tiles to the Canvas
         return this.state.board.map((tileLetter,index)=>{
-            console.log(tileLetter)
-            return <Tile index={index} letter={tileLetter} handleClick={this.handleClick} player={this.state.player}/>
+            console.log(this.gameOver())
+            return <Tile gameOver={this.gameOver()} index={index} letter={tileLetter} handleClick={this.handleClick} player={this.state.player}/>
         })
     }
+
+    gameOver(){
+        /* This will determine if game is over 
+        * Someone got a line
+        * Board is full and no one wins
+        */
+       let gameOver = false;
+       let playerWon = '';
+       this.state.winningIndices.forEach(array=>{
+           if(!gameOver){
+               // If no one has won
+               // Loop through all the winning index bundles
+               let playerOneWins = array.every(index => {
+                   // If all the indices match for player one, they win
+                   return this.state.board[index] === 'X';
+               });
+               let playerTwoWins = array.every(index => {
+                   // If all the indices match for player two, they win
+                   return this.state.board[index] === 'O';
+               });
+               if (playerOneWins || playerTwoWins) {
+                   console.log(playerOneWins ? '1' : '2')
+                   // If either player wins, return who won (1 or 2) and return gameOver ===true
+                   gameOver = true;
+                   playerWon = playerOneWins ? '1' : '2';
+
+               } else {
+                   return false;
+               }
+           }
+       })
+       return {gameOver,playerWon};
+       // Passed into Tile component 
+    }
+
     render(){
         return this.mapTiles();
     }
